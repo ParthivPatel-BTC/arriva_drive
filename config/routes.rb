cca4fe3bd05ac5f245c29238db070c2b36685e9c
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-05-12 19:01:43 +0530

    #2 - UI design for Add New Participant

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ ArrivaDrive::Application.routes.draw do
   root :to => 'static_pages#index'
   devise_for :admins
   devise_for :participants
+
+  get '/new_participant' => 'static_pages#new_participant'
 end
