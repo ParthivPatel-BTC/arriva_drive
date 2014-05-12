7981f0bdbd89b60dfee45ecf791074b2612c3e31
subhash-shah-kcspl <subhash@kcspl.co.in>
2014-05-12 13:17:59 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArrivaDrive::Application.config.secret_key_base = '0f5648498453983907688f4270127e43b653febc4dca544a08876ecd4a4688006c559fa18f52c4744ab57b8226ee7e4f2bbb0e16c3a2624d6e1463ddb9eb4b01'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArrivaDrive::Application.config.session_store :cookie_store, key: '_arriva-drive_session'
