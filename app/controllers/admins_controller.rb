d870a15cb374fb5dce7b6a5d7383f6f8408fceeb
hardik-joshi-kcspl <hardik.joshi@kcspl.co.in>
2014-05-12 15:20:46 +0530

    admin controller created

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,6 @@
+class admins_controller < ApplicationController
+  alias :dashboard :admin_panel
+
+  def admin_panel
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ArrivaDrive::Application.routes.draw do
   devise_for :admins
   devise_for :participants
+
+  scope '/admin' do
+    get 'dashboard', to: 'admins#dashboard', as: :admin_dashboard
+  end
 end
