839ae284df7cbcbc7c9f615e459fa4b3aeae86b1
subhash-shah-kcspl <subhash@kcspl.co.in>
2014-05-16 13:08:27 +0530

    Added note model with content:text attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515105020) do
+ActiveRecord::Schema.define(version: 20140516073317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,12 @@ ActiveRecord::Schema.define(version: 20140515105020) do
   end
 
   add_index "multiple_choice_questions", ["activity_id"], name: "index_multiple_choice_questions_on_activity_id", using: :btree
+
+  create_table "notes", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "participants", force: true do |t|
     t.string   "email",                  default: "",   null: false
