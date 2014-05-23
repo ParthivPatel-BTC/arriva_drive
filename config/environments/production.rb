6f59c478eae3a0bb09cdd3bb3f73c3715f726efb
subhash-shah-kcspl <subhash@kcspl.co.in>
2014-05-23 15:40:03 +0530

    updating production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ ArrivaDrive::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( participant_assets.js participant_assets.css)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
