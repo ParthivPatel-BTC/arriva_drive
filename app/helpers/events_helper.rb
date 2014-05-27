89624ef712cf546c6516b4ffcfaaee3d7fb184f5
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-05-27 17:35:01 +0530

    #67 - Make helpers for display date and time of events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -14,4 +14,12 @@ module EventsHelper
   def event_listing_class(event)
     event.is_completed? ? 'active' : ''
   end
+
+  def participant_event_date_formatted(event_date)
+    event_date.strftime('%d.%m.%Y')
+  end
+
+  def participant_event_time_formatted(event_date)
+    event_date.strftime('%I.%M')
+  end
 end
