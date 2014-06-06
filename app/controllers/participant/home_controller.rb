1e7028958808dec1ca871086560fb82b0611cb86
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-06-06 19:12:50 +0530

    #155 - Password and new User Edit Profile screen

--- app/controllers/participant/home_controller.rb
+++ app/controllers/participant/home_controller.rb
@@ -13,8 +13,18 @@ class Participant::HomeController < ApplicationController
   end
 
   def update_profile
-    if @participant.update_attributes(activity_params)
+    if params[:password].present? && !params[:participants][:photo].present?
+      password_match?
+      @participant.update_attributes(activity_params)
       redirect_to participant_dashboard_path
+    elsif params[:participants][:photo].present? && !params[:participants][:password].present?
+      @participant.update_attributes(activity_params)
+      redirect_to participant_dashboard_path
+    elsif params[:participants][:photo].present? && params[:password].present?
+      @participant.update_attributes(activity_params)
+      redirect_to participant_dashboard_path
+    else
+      render :edit_profile
     end
   end
 
@@ -28,5 +38,16 @@ class Participant::HomeController < ApplicationController
     params.require(:participants).permit(
       :photo, :password
     )
+  end
+
+  def password_match?
+    password = params[:password]
+    password_confirmation = params[:participants][:password]
+    if password != password_confirmation
+      @participant.errors.add(:password, "does not match with confirmed password")
+      false
+    else
+      true
+    end
   end
 end
