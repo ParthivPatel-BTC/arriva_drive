98bb258fc98629b17d0d383fdb9ecdfc248ebb2c
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-07-31 17:21:56 +0530

    #181 - Delete attachment files using ajax

--- app/controllers/participant/participant_attachments_controller.rb
+++ app/controllers/participant/participant_attachments_controller.rb
@@ -1,6 +1,7 @@
 class Participant::ParticipantAttachmentsController < ApplicationController
   layout 'participant'
-  before_filter :get_participant_attachments, only: [ :index ]
+  before_filter :get_participant_attachments, only: [ :index, :destroy ]
+  before_filter :find_attachment, only: [ :destroy ]
 
   def index
   end
@@ -15,6 +16,15 @@ class Participant::ParticipantAttachmentsController < ApplicationController
     end
   end
 
+  def destroy
+    respond_to do |format|
+      @attachment.destroy
+        format.js{
+        render file: 'participant/participant_attachments/attachment'
+      }
+    end
+  end
+
   private
 
   def activity_params
@@ -25,6 +35,10 @@ class Participant::ParticipantAttachmentsController < ApplicationController
 
   def get_participant_attachments
     @attachments = ParticipantAttachment.attachments(current_participant.id)
+  end
+
+  def find_attachment
+    @attachment = ParticipantAttachment.find_by_id(params[:id])
   end
 
 end
