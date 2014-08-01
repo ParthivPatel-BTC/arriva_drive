3daedeba4d7220e67cd274ad2075d96fe3cfb3f9
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-08-01 16:24:17 +0530

    #181 - Display current participant network list with checkbox for share the file

--- app/controllers/participant/participant_attachments_controller.rb
+++ app/controllers/participant/participant_attachments_controller.rb
@@ -1,7 +1,7 @@
 class Participant::ParticipantAttachmentsController < ApplicationController
   layout 'participant'
   before_filter :get_participant_attachments, only: [ :index, :destroy ]
-  before_filter :find_attachment, only: [ :destroy ]
+  before_filter :find_attachment, only: [ :destroy, :shred_participants_list ]
 
   def index
   end
@@ -25,11 +25,23 @@ class Participant::ParticipantAttachmentsController < ApplicationController
     end
   end
 
+  def shred_participants_list
+    @participants = Network.all_participants_in_network(current_participant)
+    @shared_participants = share_from_params(params[:participant_ids])
+  end
+
+  def create_shared_participants
+    shared_participants = SharedAttachment.new(activity_params)
+    if shared_participants.save
+      redirect_to participant_attachments_path
+    end
+  end
+
   private
 
   def activity_params
     params.require(:participant_attachments).permit(
-      :participant_id, :attachment
+      :participant_id, :attachment, :participant_attachment_id, participant_id: []
     )
   end
 
@@ -41,4 +53,13 @@ class Participant::ParticipantAttachmentsController < ApplicationController
     @attachment = ParticipantAttachment.find_by_id(params[:id])
   end
 
+  def share_from_params(tag_params)
+    tags = case tag_params
+      when String
+        tag_params.try(:split, ' ')
+      when Array
+        tag_params
+    end
+    tags || []
+  end
 end
