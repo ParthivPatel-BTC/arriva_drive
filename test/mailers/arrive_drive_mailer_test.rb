bc834a023b65ce1070be26b7299116cb5b5fbfd7
SubhashShah-BTC <subhash.shah@beaverlogic.com>
2014-09-10 17:30:43 +0530

    Code pushed by Tobin in Pocketworks repository

--- test/mailers/arrive_drive_mailer_test.rb
+++ test/mailers/arrive_drive_mailer_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ArriveDriveMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @bob = Participant.create(first_name: "Bob", last_name: "Smith", email: "bs@pocketworks.co.uk")
+    @paul = Participant.create(first_name: "Paul", last_name: "Roberts", email: "pr@pocketworks.co.uk")    
+  end
+  
+  test "Test network notificaiton email looks good" do
+    mail = ArriveDriveMailer.send_network_notification(@paul, @bob)
+    assert_equal "DRIVE: Welcome To My Network", mail.subject
+    #show_me_the_email(mail)
+  end
+  
+  test "Attachment notifications look good" do
+    @attachment = OpenStruct.new({id: '1',attachment_file_name: 'IGNORE_TEST'})
+    mail = ArriveDriveMailer.send_shared_notification(@attachment, @paul, @bob.email)
+    assert_equal "DRIVE: New Shared File", mail.subject
+    #show_me_the_email(mail)
+  end
+  
+  test "Test note notifications look good" do 
+    @note = Note.create(owner: @bob, content: "Hello there. This is my note.")
+    mail = ArriveDriveMailer.send_notification_to_participant(@note, @paul)
+    assert_equal "DRIVE: New Shared Note", mail.subject
+    #show_me_the_email(mail)
+  end
+  
+  def show_me_the_email(mail)     
+      File.delete('/tmp/mail.html')
+      File.open('/tmp/mail.html','w').write mail.body
+      `open /tmp/mail.html`      
+  end
 end
