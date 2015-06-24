08dea26bfe9973b9257b6ac4504ff3d95e5349e0
ParthivPatel-BTC <parthiv.patel@botreeconsulting.com>
2015-06-24 19:22:01 +0530

    MY FILES: Resolved network slider issue

--- app/assets/javascripts/participant/files.js
+++ app/assets/javascripts/participant/files.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  model_popup();
   $('#myStathalf2').circliful();
   
   $('.recentnote-slide').bxSlider({
@@ -19,36 +20,77 @@ $(document).ready(function(){
   });
   
   
-  $(".accord-toggle").click(function(){
-    $(".accord-div").slideUp();
-    if($(this).next('.accord-div').is(':visible')){
-      $(this).next('.accord-div').slideUp();
+  $("#toggle1").click(function(){
+    $("#div1").slideUp();
+    if($(this).next('#div1').is(':visible')){
+      $(this).next('#div1').slideUp();
       $(this).removeClass("active");
     }
     else{
-      $(this).next('.accord-div').slideDown();
+      $(this).next('#div1').slideDown();
       $(this).addClass("active");
     }
     
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
+    var owl1 = $('#owl1');
+    owl1.owlCarousel({
       margin:0,
       autoWidth:true,
       nav:true,
-      items:7
+      items:1,
+      autoHeight:true
       
     });
-    owl.on('mousewheel', '.owl-stage', function (e) {
+    owl1.on('mousewheel', '#owl1.owl-stage', function (e) {
       if (e.deltaY>0) {
-        owl.trigger('next.owl');
+        owl1.trigger('next.owl');
       } else {
-        owl.trigger('prev.owl');
+        owl1.trigger('prev.owl');
       }
       e.preventDefault();
     });
+    
+    owl1.data('owlCarousel').refresh();
   });
-  model_popup();
+  
   $('#participant_attachment_id').on( "click", function() {
     $('#participant_attachments_attachment').trigger( "click" );
   });
+});
+
+$(document).ready(function(){
+  $('.custom-toggle').on('click', function(){
+   parentObj = $(this).parent();
+   customCarousel = parentObj.find('.custom-carousel');
+   tagsameoneDetailObj = parentObj.find('.tagsameone-detail');
+
+    if(tagsameoneDetailObj.is(':visible')){
+      tagsameoneDetailObj.slideUp();
+      $(this).removeClass("active");
+    }
+    else{
+      tagsameoneDetailObj.slideDown();
+      $(this).addClass("active");
+    }
+
+
+    customCarousel.owlCarousel({
+      margin:0,
+      autoWidth: true,
+      nav:true,
+      items:1,
+      autoHeight: true
+    });
+  
+    customCarousel.on('mousewheel', "customCarousel.owl-stage", function (e1) {
+    if (e1.deltaY>0) {
+      customCarousel.trigger('next.owl');
+    } else {
+      customCarousel.trigger('prev.owl');
+    }
+    e1.preventDefault();
+   
+  });
+  
+});
+
 });
